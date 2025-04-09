4f2f0fefc182d436ece94149d839c1b804c16868
Brianpan <b97705021@ntu.edu.tw>
2025-04-08 20:27:16 -0700

    hotfix

--- list.c
+++ list.c
@@ -2,6 +2,14 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+size_t list_size(list_t *l)
+{
+    size_t size = 0;
+    for (list_item_t *cur = l->head; cur != NULL; cur = cur->next)
+        size++;
+    return size;
+}
+
 void list_insert_before(list_t *l,
                                       list_item_t *before,
                                       list_item_t *item)
--- list.h
+++ list.h
@@ -8,13 +8,8 @@ typedef struct {
     struct list_item *head;
 } list_t;
 
-size_t list_size(list_t *l)
-{
-    size_t size = 0;
-    for (list_item_t *cur = l->head; cur != NULL; cur = cur->next)
-        size++;
-    return size;
-}
+size_t list_size(list_t *l);
+
 
  void list_insert_before(list_t *l,
                                       list_item_t *before,
